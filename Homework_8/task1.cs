27abc669fba705a6e831250ec61f59b9e49771b3
zarifaqarayeva <z.qarayeva2000@gmail.com>
2023-03-10 18:18:03 +0400

    Task 1
    Couldn't do task 2.

---
+++ Homework_8/task1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_8
+{
+    internal class task1
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Please, enter the first number:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please, enter the second number:");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please, enter the third number:");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("First answer is " + (x + y) * z);
+            Console.WriteLine("Second answer is " + (x * y + y * z));
+            Console.ReadLine();
+
+        }
+    }
+}
